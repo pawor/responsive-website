4e90bf35ce7dc48026fed80af9fa3911c0abe6a6
pawor <pawor.emi@gmail.com>
2021-09-27 01:25:12 +0700

    first commit

---
+++ foods/script.js
@@ -0,0 +1,6 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+setInterval(() => {
+    const icon = document.querySelector('.section-1-icons i.change')
+    icon.classList.remove('change')
+    icon.nextElementSibling.classList.add('change')
+},1000)
